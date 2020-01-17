ef20f8fa226e7b76e0d98334ebac792fe16c88cb
seanjcarley <seanjcarley@outlook.com>
2020-01-17 20:32:03 +0000

    Added sendEmail.js and updated contacted.html to send email via emailjs

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,12 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "resume", {
+        "from_name": contactForm.name.value, 
+        "from_email": contactForm.emailaddress.value, 
+        "project_request": contactForm.pojectsummary.value
+    }) .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        }, function(error) {
+            console.log("FAILED", error);
+        });
+};
